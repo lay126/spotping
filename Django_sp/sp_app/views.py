73507d48e7ca00d25f2854573d18533560d52c9a
a young adelina Lee <lay126@gamil.com>
2015-01-01 02:18:54 +0900

    request_photo_update

--- Django_sp/sp_app/views.py
+++ Django_sp/sp_app/views.py
@@ -319,7 +319,7 @@ def request_photo_update(request):
 
 	coupon_category_index_ = request.POST.get('coupon_category_index')
 	coupon_product_index_ = request.POST.get('coupon_product_index')
-	coupon_photo_index_ = request.POST.get('coupon_product_index')
+	coupon_index_ = request.POST.get('coupon_index_')
 
 	if coupon_category_index_ == '1':
 		coupon_category_index_k_ = 'm_daily'
@@ -352,7 +352,7 @@ def request_photo_update(request):
 	if request.method == 'POST':
 		if 'file' in request.FILES:
 			file = request.FILES['file']
-			filename = '' + coupon_category_index_k_ + '_' + str(coupon_product_index_ )+ '_' + str(coupon_photo_index_)
+			filename = '' + coupon_category_index_k_ + '_' + str(coupon_product_index_ )+ '_' + str(coupon_index_)
 
 			try:
 				pic_ = SP_PICTURE()
@@ -366,18 +366,78 @@ def request_photo_update(request):
 
 			# get make photo index
 			pic_now = SP_PICTURE.objects.get(sp_name=filename)
-			coupon_daily_photo_index_ = pic_now.sp_photo_index
+			coupon_index_ = pic_now.sp_photo_index
 
-	# get coupon
-	coupon_ = COUPON_DAILY.objects.get(coupon_daily_name=coupon_daily_name_)
-	coupon_daily_index_ = coupon_.coupon_daily_index
+	if coupon_category_index_ == '1':
+		coupon_ = COUPON_DAILY.objects.get(coupon_daily_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_daily_index
+		coupon_.coupon_daily_index = coupon_index_
+
+	elif coupon_category_index_ == '2':
+		coupon_ = COUPON_GREENS.objects.get(coupon_greens_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_greens_index
+		coupon_.coupon_greens_index = coupon_index_
+
+	elif coupon_category_index_ == '3':
+		coupon_ = COUPON_FISH.objects.get(coupon_fish_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_fish_index
+		coupon_.coupon_fish_index = coupon_index_
+
+	elif coupon_category_index_ == '4':
+		coupon_ = COUPON_RICE.objects.get(coupon_rice_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_rice_index
+		coupon_.coupon_rice_index = coupon_index_
+
+	elif coupon_category_index_ == '5':
+		coupon_ = COUPON_MEAT.objects.get(coupon_meat_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_meat_index
+		coupon_.coupon_meat_index = coupon_index_
+
+	elif coupon_category_index_ == '6':
+		coupon_ = COUPON_EGG.objects.get(coupon_egg_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_egg_index
+		coupon_.coupon_egg_index = coupon_index_
+
+	elif coupon_category_index_ == '7':
+		coupon_ = COUPON_HAM.objects.get(coupon_ham_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_ham_index
+		coupon_.coupon_ham_index = coupon_index_
+
+	elif coupon_category_index_ == '8':
+		coupon_ = COUPON_SIDE.objects.get(coupon_side_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_side_index
+		coupon_.coupon_side_index = coupon_index_
+
+	elif coupon_category_index_ == '9':
+		coupon_ = COUPON_WATER.objects.get(coupon_water_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_water_index
+		coupon_.coupon_water_index = coupon_index_
+
+	elif coupon_category_index_ == '10':
+		coupon_ = COUPON_INSTANT.objects.get(coupon_instant_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_instant_index
+		coupon_.coupon_instant_index = coupon_index_
+
+	elif coupon_category_index_ == '11':
+		coupon_ = COUPON_ICE.objects.get(coupon_ice_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_ice_index
+		coupon_.coupon_ice_index = coupon_index_
+
+	elif coupon_category_index_ == '12':
+		coupon_ = COUPON_BAKERY.objects.get(coupon_bakery_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_bakery_index
+		coupon_.coupon_bakery_index = coupon_index_
+
+	elif coupon_category_index_ == '13':
+		coupon_ = COUPON_SNACK.objects.get(coupon_snack_index=coupon_index_)
+		coupon_index_ = coupon_.coupon_snack_index
+		coupon_.coupon_snack_index = coupon_index_
 
 	# swich coupon photo index
-	coupon_.coupon_daily_photo_index = coupon_daily_photo_index_
 	coupon_.save()
 
 	json_data = json.dumps('success save photo')
-	return HttpResponse(json_data, content_type='application/json')	
+	return HttpResponse(json_data, content_type='application/json')
 
 
 # photo download---------------------------------------------------
